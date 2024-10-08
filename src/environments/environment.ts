4a115f38e2366083de6a7cde8a6b54ef26092b7e
Yevhen <yevhen_voropaiev@epam.com>
2024-10-08 16:04:28 +0300

    #3 Azure integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'http://localhost:7071/api/',
+    product: 'https://fa-products-service-ne-004.azurewebsites.net/api',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'http://localhost:7071/api/',
