a41162e0de341bc71cb937491bb6c613b38e69a7
Yevhen <yevhen_voropaiev@epam.com>
2024-12-03 15:48:56 +0200

    #7 containers configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -315,3 +315,73 @@ resource "azurerm_servicebus_queue" "my_servicebus_queue" {
   requires_session                        = false
   dead_lettering_on_message_expiration    = false
 }
+
+resource "azurerm_resource_group" "chatbot_rg" {
+  name     = "tchatbot_rg004"
+  location = "northeurope"
+}
+
+resource "azurerm_container_registry" "chatbot_acr" {
+  name                = "${var.unique_resource_id_prefix}chatbotacr"
+  resource_group_name = azurerm_resource_group.chatbot_rg.name
+  location            = azurerm_resource_group.chatbot_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_log_analytics_workspace" "chatbot_log_analytics_workspace" {
+  name                = "${var.unique_resource_id_prefix}-log-analytics-chatbot"
+  location            = azurerm_resource_group.chatbot_rg.location
+  resource_group_name = azurerm_resource_group.chatbot_rg.name
+}
+
+resource "azurerm_container_app_environment" "chatbot_cae" {
+  name                       = "${var.unique_resource_id_prefix}-cae-chatbot"
+  location                   = azurerm_resource_group.chatbot_rg.location
+  resource_group_name        = azurerm_resource_group.chatbot_rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.chatbot_log_analytics_workspace.id
+}
+
+resource "azurerm_container_app" "chatbot_ca_docker_acr" {
+  name                         = "${var.unique_resource_id_prefix}-ca-acr"
+  container_app_environment_id = azurerm_container_app_environment.chatbot_cae.id
+  resource_group_name          = azurerm_resource_group.chatbot_rg.name
+  revision_mode                = "Single"
+
+  registry {
+    server               = azurerm_container_registry.chatbot_acr.login_server
+    username             = azurerm_container_registry.chatbot_acr.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 3000
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+
+  }
+
+  template {
+    container {
+      name   = "${var.unique_resource_id_prefix}-chatbot-container-acr"
+      image  = "${azurerm_container_registry.chatbot_acr.login_server}/${var.chatbot_container_name}:${var.chatbot_container_tag_acr}"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name  = "CONTAINER_REGISTRY_NAME"
+        value = "Azure Container Registry"
+      }
+    }
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.chatbot_acr.admin_password
+  }
+}
